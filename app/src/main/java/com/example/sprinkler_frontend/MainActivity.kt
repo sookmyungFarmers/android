1bd1338814f59b568d5421fe0a74215ea5d1f430
haxxnshine <haeun728@sookmyung.ac.kr>
2025-02-26 13:10:16 +0900

    feat: bottom navigation 구현

--- app/src/main/java/com/example/sprinkler_frontend/MainActivity.kt
+++ app/src/main/java/com/example/sprinkler_frontend/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.sprinkler_frontend
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // 첫 화면을 HomeFragment로 설정
+        replaceFragment(HomeFragment())
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.questionFragment -> replaceFragment(QuestionFragment())
+                R.id.reviewFragment -> replaceFragment(ReviewFragment())
+                R.id.friendsFragment -> replaceFragment(FriendsFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+            }
+            true
         }
+    }
+
+    // 프래그먼트를 변경하는 함수
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
